d8534296c2fff5a9098ce540f8bde078893ed534
Sridhar Ratnakumar <srid@srid.ca>
2023-07-24 15:56:59 -0400

    Fix storage.nix to work on macOS

--- nix/storage.nix
+++ nix/storage.nix
@@ -1,11 +1,15 @@
 # Keep CI machines from getting run out of disk space.
+{ pkgs, ... }:
 {
   nix = {
     settings.auto-optimise-store = true;
     gc = {
       automatic = true;
-      dates = "weekly";
       options = "--delete-older-than 30d";
-    };
+    } // (if pkgs.stdenv.isLinux then {
+      dates = "weekly";
+    } else {
+      interval = { Hour = 0; Minute = 0; };
+    });
   };
 }
