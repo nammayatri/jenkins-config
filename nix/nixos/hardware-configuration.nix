18a86a1ac6eef7c9c44833104d538fc4810e9cb8
Sridhar Ratnakumar <srid@srid.ca>
2023-09-06 12:29:07 -0400

    Increase swap space (not deployed yet)

--- nix/nixos/hardware-configuration.nix
+++ nix/nixos/hardware-configuration.nix
@@ -30,7 +30,7 @@
 
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 32*1024;
+    size = 64*1024;
   } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
